245669bd37d75b8c7654a6e5e2abd88ae604c3ba
Arief Taufik Rahman <eddiesti@Ariefs-MacBook-Air.local>
2025-02-27 09:26:01 +0700

    Make city resource

--- app/Filament/Resources/CityResource.php
+++ app/Filament/Resources/CityResource.php
@@ -6,12 +6,17 @@ use App\Filament\Resources\CityResource\Pages;
 use App\Filament\Resources\CityResource\RelationManagers;
 use App\Models\City;
 use Filament\Forms;
+use Filament\Forms\Components\FileUpload;
+use Filament\Forms\Components\TextInput;
 use Filament\Forms\Form;
 use Filament\Resources\Resource;
 use Filament\Tables;
+use Filament\Tables\Columns\ImageColumn;
+use Filament\Tables\Columns\TextColumn;
 use Filament\Tables\Table;
 use Illuminate\Database\Eloquent\Builder;
 use Illuminate\Database\Eloquent\SoftDeletingScope;
+use Illuminate\Support\Str;
 
 class CityResource extends Resource
 {
@@ -24,6 +29,20 @@ class CityResource extends Resource
         return $form
             ->schema([
                 //
+                FileUpload::make('image')
+                    ->image()
+                    ->directory('cities')
+                    ->required()
+                    ->columnSpan(2),
+                TextInput::make('name')
+                    ->required()
+                    ->debounce(500)
+                    ->reactive()
+                    ->afterStateUpdated(function ($state, callable $set){
+                        $set('slug', Str::slug($state));
+                    }),
+                TextInput::make('slug')
+                    ->required(),
             ]);
     }
 
@@ -32,12 +51,17 @@ class CityResource extends Resource
         return $table
             ->columns([
                 //
+                ImageColumn::make('image'),
+                TextColumn::make('name'),
+                TextColumn::make('slug'),
             ])
             ->filters([
                 //
             ])
             ->actions([
+                Tables\Actions\ViewAction::make(),
                 Tables\Actions\EditAction::make(),
+                Tables\Actions\DeleteAction::make(),
             ])
             ->bulkActions([
                 Tables\Actions\BulkActionGroup::make([
