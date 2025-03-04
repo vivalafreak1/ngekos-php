e8a4dfbb6f279da12affaa1caae6d43e01c2697d
Eddiesti <51298768+vivalafreak1@users.noreply.github.com>
2025-03-04 14:52:16 +0700

    Repository

---
+++ app/Interfaces/BoardingHouseRepositoryInterface.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace App\Interfaces;
+
+interface BoardingHouseRepositoryInterface
+{
+    public function getAllBoardingHouses($search = null, $city = null, $category = null);
+
+    public function getPopularBoardingHouses($limit = 5);
+
+    public function getBoardingHouseByCitySlug($slug);
+
+    public function getBoardingHouseByCategorySlug($slug);
+
+    public function getBoardingHouseBySlug($slug);
+
+}
