8c2015d330a2797ed4427349c0e10a4058693a6e
Eddiesti <51298768+vivalafreak1@users.noreply.github.com>
2025-03-10 16:56:04 +0700

    Add bottom navigation functionality

---
+++ app/Http/Controllers/BoardingHouseController.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Interfaces\BoardingHouseRepositoryInterface;
+use App\Interfaces\CategoryRepositoryInterface;
+use App\Interfaces\CityRepositoryInterface;
+use Illuminate\Http\Request;
+
+class BoardingHouseController extends Controller
+{
+    //
+
+    private CityRepositoryInterface $cityRepository;
+    private CategoryRepositoryInterface $categoryRepository;
+    private BoardingHouseRepositoryInterface $boardingHouseRepository;
+
+    public function __construct(
+        CityRepositoryInterface $cityRepository,
+        CategoryRepositoryInterface $categoryRepository,
+        BoardingHouseRepositoryInterface $boardingHouseRepository
+    ) {
+        $this->cityRepository = $cityRepository;
+        $this->categoryRepository = $categoryRepository;
+        $this->boardingHouseRepository = $boardingHouseRepository;
+    }
+
+    public function find()
+    {
+        $categories = $this->categoryRepository->getAllCategories();
+        $cities = $this->cityRepository->getAllCities();
+
+        return view('pages.boarding-house.find', compact('categories', 'cities'));
+    }
+}
--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,12 @@
 <?php
 
+use App\Http\Controllers\BoardingHouseController;
+use App\Http\Controllers\BookingController;
 use App\Http\Controllers\HomeController;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', [HomeController::class, 'index']);
+Route::get( '/', [HomeController::class, 'index'])->name('home');
+
+Route::get( '/find-kos', [BoardingHouseController::class, 'find'])->name('find-kos');
+
+Route::get('/check-booking', [BookingController::class, 'check'])->name('check-booking');
