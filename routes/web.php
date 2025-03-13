1707b7ab3e2469c152d4b3ad1f925ed86db45bfc
Eddiesti <eddiesti@Ariefs-MacBook-Air.local>
2025-03-13 09:37:37 +0700

    Find by category

--- routes/web.php
+++ routes/web.php
@@ -2,12 +2,16 @@
 
 use App\Http\Controllers\BoardingHouseController;
 use App\Http\Controllers\BookingController;
+use App\Http\Controllers\CategoryController;
 use App\Http\Controllers\HomeController;
 use Illuminate\Support\Facades\Route;
 
 Route::get( '/', [HomeController::class, 'index'])->name('home');
 
-Route::get( '/find-kos', [BoardingHouseController::class, 'find'])->name('find-kos');
+Route::get( '/category/{slug}', [CategoryController::class, 'show'])->name(name: 'category.show');
+
+Route::get( '/find-kos', [BoardingHouseController::class, 'find'])->name(name: 'find-kos');
+
 Route::get('/find-results', [BoardingHouseController::class, 'findResults'])->name('find-kos.results');
 
 Route::get('/check-booking', [BookingController::class, 'check'])->name('check-booking');
